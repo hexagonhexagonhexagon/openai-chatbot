fceb5e16f08c0c8985dee77d7d74e61822ccb71a
Fletcher Watson <fletcherwatson@Fletchers-Air.local.meter>
2023-11-01 18:35:28 -0500

    created while loop and moved code inside loop. can't figure out how to append the messages to the chat history so that the conversation with the chatbot is fluid. it keeps going back to the initial userMessage and starting over.

--- openai.php
+++ openai.php
@@ -10,37 +10,39 @@ $dotenv->load();
 $yourAPIKey = $_ENV['OPENAI_PHP_CHATBOT_API_KEY'];
 $client = OpenAI::client($yourAPIKey);
 
-$chatBotName = readline("Hello, friend. I'm your personal helpful chatbot. What would you like to call me? ");
-$userName = readline("Okay, from now on you can call me $chatBotName. What is your name? ");
-echo "Hello, $userName. It's nice to meet you. ";
-$initialUserMessage = readline("Tell me, $userName, how are you feeling today? You can tell me anything you want.");
 
-$chatBotSystemMessage = "You are a friendly chatbot named $chatBotName whose main objective is to help the user have encouraging but helpful conversations about themselves, their life, and any challengesthat might be in their life at the moment. Consider yourself to be a friend of the user, and you want to help them feel better about themselves.";
 
-$response = $client->chat()->create([
-    'model' => 'gpt-3.5-turbo',
-    'messages' => [
-        ['role' => 'system', 'content' => $chatBotSystemMessage],
-        ['role' => 'user', 'content' => $initialUserMessage]
-    ],
-]);
+while (true) {
+    $chatBotSystemMessage = "You are a friendly chatbot who is here to help people with their problems. Be kind, thoughtful, and ask simple questions for the purpose of helping the user identify why they are not feeling well, and then kindly propose ideas and solutions to make them feel better, or to fix their problems.";
+    $initialAssistantMessage = "Tell me, how are you feeling today? You can tell me anything you want.";
+    $userMessage = readline($initialAssistantMessage);
+    $response = $client->chat()->create([
+        'model' => 'gpt-3.5-turbo',
+        'messages' => [
+            ['role' => 'system', 'content' => $chatBotSystemMessage],
+            ['role' => 'user', 'content' => $userMessage]
+        ]
+        ]);
 
-$response->id; // 'chatcmpl-6pMyfj1HF4QXnfvjtfzvufZSQq6Eq'
-$response->object; // 'chat.completion'
-$response->created; // 1677701073
-$response->model; // 'gpt-3.5-turbo-0301'
+    $response->id; // 'chatcmpl-6pMyfj1HF4QXnfvjtfzvufZSQq6Eq'
+    $response->object; // 'chat.completion'
+    $response->created; // 1677701073
+    $response->model; // 'gpt-3.5-turbo-0301'
 
-foreach ($response->choices as $result) {
-    $result->index; // 0
-    $result->message->role; // 'assistant'
-    $result->message->content; // '\n\nHello there! How can I assist you today?'
-    $result->finishReason; // 'stop'
-}
+    foreach ($response->choices as $result) {
+        $result->index; // 0
+        $result->message->role; // 'assistant'
+        $result->message->content; // '\n\nHello there! How can I assist you today?'
+        $result->finishReason; // 'stop'
+    }
 
-$response->usage->promptTokens; // 9,
-$response->usage->completionTokens; // 12,
-$response->usage->totalTokens; // 21
+    $response->usage->promptTokens; // 9,
+    $response->usage->completionTokens; // 12,
+    $response->usage->totalTokens; // 21
 
-$response->toArray(); // ['id' => 'chatcmpl-6pMyfj1HF4QXnfvjtfzvufZSQq6Eq', ...]
-echo $response->choices[0]->message->content;
+    $response->toArray(); // ['id' => 'chatcmpl-6pMyfj1HF4QXnfvjtfzvufZSQq6Eq', ...]
+    
+    $chatBotResponse = $response->choices[0]->message->content;
+    $userMessage = readline($chatBotResponse);
+};
 
