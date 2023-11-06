e7e2769880f14a617eb30e38d86ca622379ea959
Fletcher Watson <fletcherwatson@Fletchers-Air.local.meter>
2023-11-06 10:00:18 -0600

    updated while loop to stop when user types quit. removed unnecessary variables.

--- openai.php
+++ openai.php
@@ -10,19 +10,27 @@ $dotenv->load();
 $yourAPIKey = $_ENV['OPENAI_PHP_CHATBOT_API_KEY'];
 $client = OpenAI::client($yourAPIKey);
 
-$chatBotSystemMessage = "You are a friendly chatbot who is here to help people with their problems.
+$chatBotSystemMessage = 
+"You are a friendly chatbot who is here to help people with their problems.
 Be kind, thoughtful, and ask simple questions for the purpose of helping the user identify why they
 are not feeling well, and then kindly propose ideas and solutions to make them feel better,
-or to fix their problems.";
+or to fix their problems. Do not be rude, mean, or condescending. Allow the user to dictate when the conversation
+is over. If the user says 'quit', then end the conversation. Inform the user that the conversation is over when
+the user says 'quit' early in the conversation with the user.";
 
-$initialAssistantMessage = "Hello. I am here to help. How are you feeling today? You can tell me anything you want.";
+// initialize the user message to an empty string
 $userMessage = "";
 
+// initialize the messages array to contain the chatbot system message as the first message
 $messages = [
     ['role' => 'system', 'content' => $chatBotSystemMessage]
 ];
 
-while (true) {
+# create a while loop that controls the chat conversation with the user and the chatbot
+# the while loop should run as long as the userMessage is not equal to 'quit'
+# inside the while loop, create a $response variable that calls the chat() method on the $client object
+# change the 
+while ($userMessage != 'quit') {
     $response = $client->chat()->create([
         'model' => 'gpt-3.5-turbo',
         'messages' => $messages
@@ -51,6 +59,5 @@ while (true) {
 
     array_push($messages, ['role' => 'assistant', 'content' => $chatBotResponse]);
     array_push($messages, ['role' => 'user', 'content' => $userMessage]);
-    echo json_encode($messages, JSON_PRETTY_PRINT);
 };
 
